7d0a944ba276d98ab6ec7840b214d9a7ce7b4142
Jordan Waddon <jordanwaddon98@gmail.com>
2019-06-22 19:01:53 +0100

    Delete ModBlocks.java

--- main/java/com/megarapidz/discoverycraft/blocks/ModBlocks.java
+++
@@ -1,8 +0,0 @@
-package com.megarapidz.discoverycraft.blocks;
-
-import net.minecraftforge.registries.ObjectHolder;
-
-public class ModBlocks {
-    @ObjectHolder("discoverycraft:dragonstoneblock")
-    public static DragonstoneBlock DRAGONSTONEBLOCK;
-}
